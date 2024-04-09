cd90dcceeba43876b04fde81ab3808083d019d82
jayeshsinghstaqo <jayesh.singh@staqo.com>
2024-04-09 13:59:40 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 
